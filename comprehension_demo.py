40b4996520a03a325b0f8ae68c6efeb9e8d11f20
Francisco Molina <franciscomoma@gmail.com>
2021-03-18 18:02:09 +0100

    Cosas de la clase

---
+++ comprehension_demo.py
@@ -0,0 +1,10 @@
+from examples.scrapper import obtener_noticias
+import json
+
+resultado, numero_peticiones = obtener_noticias()
+
+categorias = {noticia["categoria"] for noticia in resultado}
+
+titulos_por_autor = {autor: [noticia["titulo"] for noticia in resultado if noticia["autor"] == autor] for autor in {noticia["autor"] for noticia in resultado}}
+
+print(json.dumps(titulos_por_autor, indent=4))
---
+++ modelos.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+from examples.scrapper import obtener_noticias
+
+print(obtener_noticias()[0])
+
